5bcae40351ea21cfa733d72e55014716e72bc64c
FuZuoJun <91184331+FuZuoJun@users.noreply.github.com>
2025-05-16 01:11:44 +0800

    Update view.php

--- midterm/view.php
+++ midterm/view.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($_GET['jobId'])) {
-    echo "請提供 jobId 參數";
+    echo "請提供 jobId";
     exit;
 }
 $jobId = $_GET['jobId'];
@@ -9,6 +9,6 @@ $resultFile = "/share/results/$jobId.txt";
 if (file_exists($resultFile)) {
     echo nl2br(file_get_contents($resultFile));
 } else {
-    echo "任務尚未完成，請稍後再試。";
+    echo "任務尚未完成，請稍候再試。";
 }
 ?>
